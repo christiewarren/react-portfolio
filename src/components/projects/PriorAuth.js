55f3d9707324bc7cc83eae6dc2c35def2d04e6c0
Christie Warren <cewarren801@gmail.com>
2024-10-16 18:17:40 -0700

    eng collab, glossary, what's next

--- src/components/projects/PriorAuth.js
+++ src/components/projects/PriorAuth.js
@@ -55,21 +55,27 @@ export default function PriorAuth(){
          </section>
          </ScrollAnimation>
 
-            <span className='section-divider' id='case-study'><h3>Case Study</h3><hr/></span>
+         <span className='section-divider' id='case-study'><h3>Case Study</h3><hr/></span>
          
          <ScrollAnimation animateIn="animate__fadeInUp-New" duration={1} animateOnce={true} delay={0}>
          <section class='case-study-section context'>
             <h4>Context</h4>
             <div className='right'>
-               <p>Alma is a platform for therapists (mental health care providers) to run private practices on without much of the burden typically associated with doing so. A lot of the value of Alma brings is centered around insurance: credentialing providers with insurance companies and negotiating higher rates, investigating and correcting claims, verifying patients' benefits, etc.</p> 
-               <p>Another insurance process Alma handles on a providers' behalves is getting authorization from insurance companies to conduct psychological and neuropsychological testing. Providers submit the details of the testing they intend to conduct, and Alma staff determine 1) whether authorization is required and 2) if so, whether the insurance company approves it.</p>  
+               <h5>Glossary</h5>
+               <p><strong>Providers:</strong> Alma's primary users; providers of mental health care (therapists, psychologists, etc.)</p>
+               <p><strong>Prior authorization ("prior auth") request:</strong> A request from a provider to conduct psychological or neuropsychological testing with their patient</p>
+
+               <h5>What Alma Does</h5>
+               <p>Alma is a platform for providers to run private practices on without much of the burden typically associated with doing so. A lot of the value Alma brings is centered around insurance: credentialing providers with insurance companies and negotiating higher rates, investigating and correcting claims, verifying patients' benefits, etc.</p> 
+               <p>Another insurance process Alma handles on a providers' behalves is getting authorization from insurance companies to conduct psychological and neuropsychological testing (prior auth). Providers submit the details of the testing they intend to conduct, and Alma staff determine 1) whether prior auth is required for their case and 2) if so, whether the insurance company approves it.</p>  
+               
                <h5>The Problem</h5>
-               <p>Prior to this project, prior authorization (prior auth) requests at Alma were handled entirely through Asana tickets, with outcomes being written in the comments. The biggest problem with this manual process was a <span className='highlight'>lack of stored and structured data,</span> which prevented us from:
+               <p>Prior to this project, prior auth requests at Alma were handled entirely through Asana tickets, with outcomes being written in the comments. The biggest problem with this manual process was a <span className='highlight'>lack of stored and structured data,</span> which prevented us from:
                </p>
                <ul>
-               <li><p>Automating processes that rely on this data downstream</p></li>
-               <li><p>Detecting "bad" provider behavior before they act on it</p></li>
-               <li><p>Another item?</p></li>
+                  <li><p>Automating tedious processes that rely on this data downstream</p></li>
+                  <li><p>Detecting "bad" provider behavior before they act on it</p></li>
+                  <li><p>Another item?</p></li>
                </ul>
             </div>
          </section>
@@ -80,7 +86,7 @@ export default function PriorAuth(){
 
             <h4>Research &amp; Diagramming</h4>
             <div className='right'>
-            <h6>Realizing that prior authorizations were a complex, multi-team effort, I wanted to start by mapping out the existing processes. I believe this step is absolutely crucial: not only did it help me learn the many nuances, but the diagrams I made also became an invaluable resource for giving engineers, stakeholders, and design teammates a quick (or thorough) rundown.</h6>
+            <h6>Realizing that prior authorizations were a complex, multi-team effort, I started by mapping out the existing processes. I believe this step is absolutely crucial: not only did it help me learn the many nuances, but the diagrams I made also became an invaluable resource for giving engineers, stakeholders, and design teammates a quick (or thorough) rundown.</h6>
             <br/><small>Designing in the world of insurance and internal tools, I've learned it's not enough for me to be intimately familiar with a workflow; I also need to be able to distill that nuance down and communicate it in, for example, a 20 minute design critique. And one thing's for sure: <span className='highlight'>visuals &#x1F44F; always &#x1F44F; help &#x1F44F;</span></small>
             <br/><h5>User Interviews / Shadowing</h5>
             <p>My PM and I held 5 "shadow sessions" with future internal users, where they walked us through their day to day prior auth workflows. We asked many questions, learning not only from their expertise, but also from their pains and frustrations with the process.</p>
@@ -120,41 +126,27 @@ export default function PriorAuth(){
                {/* <img src='./assets/search/mixpanel-graph.png' className='img-shadow' alt="mixpanel graph of search data" /> */}
                <small>image of lofi and initial slideouts</small>
             </div>
-            <p>However, feedback from design critiques helped me refocus on the goal of efficiency and push outside the box, without straying from our design system. The next iterations gave prior auth requests their own dedicated page with inline editing capabilities. This allows plenty of room for all the request and reference data and keeps the user in context as they complete tasks.</p>
+            <p>However, feedback from design critiques helped me refocus on the goal of efficiency and <span className='highlight'>push outside the box, without straying from our design system.</span> The next iterations gave prior auth requests their own dedicated page with inline editing capabilities. This allows plenty of room for all the request and reference data and keeps the user in context as they complete tasks.</p>
             <div className='img-wrap'>
                {/* <img src='./assets/search/mixpanel-graph.png' className='img-shadow' alt="mixpanel graph of search data" /> */}
                <small>image of full page design (without extra data?)</small>
             </div>
             <h5>Flexibility vs Guidance</h5>
             <br/><p>Maintaining flexibility proved to be a surprisingly challenging goal. In most cases, I kept all fields editable, regardless of the request's status or the user's presumed step, which led to feedback that the flow felt too open ended and without guidance.</p>
-            <p>I experimented with dynamic CTAs to hint at likely next steps, but this neglected edge cases and the copy felt misleading. This unlocked an important idea though: using the request's status to inform guidance on next steps.</p>
+            <p>I experimented with dynamic CTAs to hint at likely next steps, but this neglected edge cases, and the copy felt misleading. This unlocked an important idea though: using the request's status to inform guidance on next steps.</p>
             <div className='img-wrap'>
                {/* <img src='./assets/search/mixpanel-graph.png' className='img-shadow' alt="mixpanel graph of search data" /> */}
                <small>image of dynamic ctas</small>
             </div>
             <p>After <strong>a lot</strong> of iteration, I landed on this floating, fixed "Next steps" callout. Considerations for this seemingly simple component included:</p>
             <ul>
-               <li><p><strong>Context:</strong> No in-page position seemed quite right, as it's dynamic content can refer to various sections. Rather than being part of the page, this positioning lets it act as commentary on the page itself.</p></li>
-               <li><p><strong>Prominence:</strong> The floating also position lets it stand out without interrupting the user's flow or the page hierarchy. It's also collapsible for those users more accustomed with the process.</p></li>
+               <li><p><strong>Context:</strong> No in-page position seemed quite right, as it's dynamic content can refer to various sections. Rather than being part of the page, this positioning lets it act as <span className='highlight'>commentary on the page itself.</span></p></li>
+               <li><p><strong>Prominence:</strong> The floating also position lets it stand out without interrupting the user's flow or the page hierarchy. It's also collapsible for the users more accustomed with the process.</p></li>
                <li><p><strong>Implementation:</strong> Our design system doesn't have this exact component, but I kept scope manageable by using our existing banner and announcement components as foundations.</p></li>
             </ul>
             <div className='img-wrap'>
                {/* <img src='./assets/search/mixpanel-graph.png' className='img-shadow' alt="mixpanel graph of search data" /> */}
                <small>carousel: gif of scrolling with "next steps", status mapping, layout iterations</small>
-            </div>
-            </div>
-            </section>
-            </ScrollAnimation>
-
-            <ScrollAnimation animateIn="animate__fadeInUp-New" duration={1} animateOnce={true} delay={0}>
-            <section class='case-study-section'>
-            <h4>Scoping</h4>
-            <div className='right'>
-            <h5>Layout & Editing</h5>
-            <p>Initially, I gravitated towards the UI patterns used in most of our internal workflows: slideouts (drawers) and modals. Knowing this project would involve a hefty implementation, I wanted to rely as much as possible on existing patterns and components.</p>
-            <div className='img-wrap'>
-               {/* <img src='./assets/search/mixpanel-graph.png' className='img-shadow' alt="mixpanel graph of search data" /> */}
-               <small>image of lofi and initial slideouts</small>
             </div>
             </div>
             </section>
@@ -210,13 +202,36 @@ export default function PriorAuth(){
 
             <ScrollAnimation animateIn="animate__fadeInUp-New" duration={1} animateOnce={true} delay={0}>
             <section class='case-study-section'>
-            <h4>What's Next?</h4>
+            <h4>Eng Collaboration</h4>
             <div className='right'>
-            <h5>Layout & Editing</h5>
-            <p>Initially, I gravitated towards the UI patterns used in most of our internal workflows: slideouts (drawers) and modals. Knowing this project would involve a hefty implementation, I wanted to rely as much as possible on existing patterns and components.</p>
+            <h5>Handoff</h5>
+            <br/><br/><small>Note: Collaboration with eng certainly didn't start at handoff, these are just some highlights.</small>
+            <p>As with any project, my design handoff included annotations like these throughout the figma file detailing various functionality: data sources, character limits, form logic, error states, etc.</p>
             <div className='img-wrap'>
                {/* <img src='./assets/search/mixpanel-graph.png' className='img-shadow' alt="mixpanel graph of search data" /> */}
-               <small>image of lofi and initial slideouts</small>
+               <small>image of annotation and blurb</small>
+            </div>
+            <p>Since the steps of this flow can be tricky to learn, I also wrote blurbs to contextualize what the user's actions and goals are at each stage.</p>
+            <p>At the initial handoff point, I recorded a walkthrough (I &#128155; Loom) of the designs and the workflow diagram in addition to doing a live walkthrough/Q&A with the engineers.</p>
+            <p>I created a weekly project sync for updates, design q's and implementation discussions, and we maintain a tight communication loop through Figma comments and Slack.</p>
+            <h5>Scoping</h5>
+            <p>My PM and I worked together early on to define 3 milestones for this project, considering what a large design and engineering effort it would be. Each milestone could stand on its own and would make progress toward an ideal state.</p>
+            <p>I also documented detailed user stories that the engineers used to estimate initial levels of effort and a rough timeline. However, almost all our engineers were quite new to the codebase, being recent hires at the time, and our newly-formed team had gaps in our process. Partway through the project, we realized these factors had led to a significant underestimation of LOE.</p>
+            <p>Luckily, our tight communication loop helped us react quickly: I worked with our lead FE engineer to identify small pieces throughout the flow that weren't essential for release, and reduced the time to MVP by ____.</p>
+            </div>
+            </section>
+            </ScrollAnimation>
+
+            <ScrollAnimation animateIn="animate__fadeInUp-New" duration={1} animateOnce={true} delay={0}>
+            <section class='case-study-section'>
+            <h4>What's Next?</h4>
+            <div className='right'>
+            <h6>Now that all this prior authorization data will be recorded and stored in our database, the next step is to do something with it!</h6>
+            <p>In the current workflow, when a provider submits a claim containing psychological testing services, a staff member checks whether they submitted an authorization request, if so, what the outcome was, and how that compares to the services on the claim. Now, we can work to automate this process by adding logic that checks the stored request data. This means that staff member's time can be put to better use on less tedious work.</p>
+            <p>Plus, we can better educate providers about prior authorization in context and prevent them from delivering care that a patient won't be covered for.</p>
+            <div className='img-wrap'>
+               {/* <img src='./assets/search/mixpanel-graph.png' className='img-shadow' alt="mixpanel graph of search data" /> */}
+               <small>image of appt slideout warning</small>
             </div>
             </div>
             </section>
