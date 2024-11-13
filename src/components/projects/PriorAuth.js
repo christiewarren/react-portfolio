cbf72eec38db2c33dbbf4274aa6b4e2d0ce30495
Christie <christie@macbook.local>
2024-11-12 21:07:12 -0800

    most prior auth images and some fine tuning

--- src/components/projects/PriorAuth.js
+++ src/components/projects/PriorAuth.js
@@ -32,9 +32,8 @@ export default function PriorAuth(){
                   <br/>2 BE Devs
                   </p>
                   <p><b>Stakeholders:</b>
-                  <br/>CEO
-                  <br/>CPO/CGO
-                  <br/>Director of Marketing
+                  <br/>Multiple Operations teams
+                  <br/>Customer support
                </p>
             </div>
          </div>
@@ -58,33 +57,29 @@ export default function PriorAuth(){
          <span className='section-divider' id='case-study'><h3>Case Study</h3><hr/></span>
          
          <ScrollAnimation animateIn="animate__fadeInUp-New" duration={1} animateOnce={true} delay={0}>
-         <section class='case-study-section context'>
-            <h4>Context</h4>
+         <section className='case-study-section context'>
+            <h4 id='context'>Context</h4>
             <div className='right'>
-               <h5>Glossary</h5>
-               <p><strong>Providers:</strong> Alma's primary users; providers of mental health care (therapists, psychologists, etc.)</p>
-               <p><strong>Prior authorization ("prior auth") request:</strong> A request from a provider to conduct psychological or neuropsychological testing with their patient</p>
 
                <h5>What Alma Does</h5>
-               <p>Alma is a platform for providers to run private practices on without much of the burden typically associated with doing so. A lot of the value Alma brings is centered around insurance: credentialing providers with insurance companies and negotiating higher rates, investigating and correcting claims, verifying patients' benefits, etc.</p> 
-               <p>Another insurance process Alma handles on a providers' behalves is getting authorization from insurance companies to conduct psychological and neuropsychological testing (prior auth). Providers submit the details of the testing they intend to conduct, and Alma staff determine 1) whether prior auth is required for their case and 2) if so, whether the insurance company approves it.</p>  
+               <p>Alma is a platform for mental health care providers (therapists, psychologists, etc.) to run private practices on. The platform reduces much of the administrative burden that typically comes with running a private practice: getting credentialed to take insurance, investigating/correcting claims, verifying patient benefits, etc.</p> 
+               <p>Another insurance process Alma handles on a provider's behalf is getting authorization from insurance companies to conduct psychological and neuropsychological testing (prior authorization, or, "prior auth"). Providers submit the details of the testing they intend to conduct (a prior auth request), and Alma staff determine 1) whether prior auth is required for their case and 2) if so, whether the insurance company approves it.</p>  
                
                <h5>The Problem</h5>
-               <p>Prior to this project, prior auth requests at Alma were handled entirely through Asana tickets, with outcomes being written in the comments. The biggest problem with this manual process was a <span className='highlight'>lack of stored and structured data,</span> which prevented us from:
+               <p>Before to this project, prior auth requests at Alma were handled entirely through Asana tickets, with outcomes being written in the comments. The biggest problem with this manual process was a <span className='highlight'>lack of stored and structured data,</span> which meant:
                </p>
-               <ul>
-                  <li><p>Automating tedious processes that rely on this data downstream</p></li>
-                  <li><p>Detecting "bad" provider behavior before they act on it</p></li>
-                  <li><p>Another item?</p></li>
-               </ul>
+               <ol>
+                  <li><p>Processes that rely on this data downstream were fully manual and tedious</p></li>
+                  <li><p>We couldn't prevent providers from delivering care that wouldn't be covered by insurance (they'd only find out once they submitted a claim)</p></li>
+               </ol>
             </div>
          </section>
          </ScrollAnimation>
 
          <ScrollAnimation animateIn="animate__fadeInUp-New" duration={1} animateOnce={true} delay={0}>
-         <section class='case-study-section'>
+         <section className='case-study-section'>
 
-            <h4>Research &amp; Diagramming</h4>
+            <h4 id='research'>Research &amp; Diagramming</h4>
             <div className='right'>
             <h6>Realizing that prior authorizations were a complex, multi-team effort, I started by mapping out the existing processes. I believe this step is absolutely crucial: not only did it help me learn the many nuances, but the diagrams I made also became an invaluable resource for giving engineers, stakeholders, and design teammates a quick (or thorough) rundown.</h6>
             <div className='learning-outer'>
@@ -103,7 +98,7 @@ export default function PriorAuth(){
                <tbody>
                <tr>
                   <td>Flexibility is key</td>
-                  <td>While prior auth typically follows one of a couple paths, exceptions are inevitable. Users need the flexibility to take steps in any order and make changes to all data points.</td>
+                  <td>While prior auth typically follows one of a couple paths, exceptions are inevitable. Internal users need the flexibility to take steps in any order and make changes to all data points.</td>
                </tr>
                <tr>
                   <td>Consolidation = efficiency</td>
@@ -120,37 +115,55 @@ export default function PriorAuth(){
             </ScrollAnimation>
 
             <ScrollAnimation animateIn="animate__fadeInUp-New" duration={1} animateOnce={true} delay={0}>
-            <section class='case-study-section'>
-            <h4>Design Evolution: Internal</h4>
+            <section className='case-study-section'>
+            <h4 id='design-evolution'>Design Evolution: Internal</h4>
             <div className='right'>
             <h5>Layout & Editing</h5>
-            <p>Initially, I gravitated towards the UI patterns used in most of our internal workflows: slideouts (drawers) and modals. Knowing this project would involve a hefty implementation, I relied as much as possible on existing patterns and components.</p>
+            <p>Initially, I gravitated towards the UI patterns in most of our internal workflows: slideouts (drawers) and modals. Knowing this project would involve significant engineering effort, I relied as much as possible on existing patterns and components.</p>
             <div className='img-wrap'>
-               {/* <img src='./assets/search/mixpanel-graph.png' className='img-shadow' alt="mixpanel graph of search data" /> */}
-               <small>image of lofi and initial slideouts</small>
+               <img src='./assets/priorauth/lofi-v0.png'  alt="lo-fi and v0 explorations with slideouts and modals" />
             </div>
             <p>However, feedback from design critiques helped me refocus on the goal of efficiency and <span className='highlight'>push outside the box, without straying from our design system.</span> The next iterations gave prior auth requests their own dedicated page with inline editing capabilities. This allows plenty of room for all the request and reference data and keeps the user in context as they complete tasks.</p>
-            <div className='img-wrap'>
-               {/* <img src='./assets/search/mixpanel-graph.png' className='img-shadow' alt="mixpanel graph of search data" /> */}
-               <small>image of full page design (without extra data?)</small>
+            <div className='img-wrap scrollable-img'>
+               <img src='./assets/priorauth/full-page.png' className='img-shadow' alt="prior auth full page" />
             </div>
             <h5>Flexibility vs Guidance</h5>
             <br/><p>Maintaining flexibility proved to be a surprisingly challenging goal. In most cases, I kept all fields editable, regardless of the user's presumed step, which led to feedback that the flow felt too open ended and without guidance.</p>
             <p>I experimented with dynamic CTAs to hint at likely next steps, but this neglected edge cases, and the copy felt misleading. This unlocked an important idea though: using the request's status to inform guidance on next steps.</p>
             <div className='img-wrap'>
-               {/* <img src='./assets/search/mixpanel-graph.png' className='img-shadow' alt="mixpanel graph of search data" /> */}
-               <small>image of dynamic ctas</small>
+               <img src='./assets/priorauth/dynamic-ctas.png' className='sm-img' alt="prior auth full page" />
             </div>
             <p>After <strong>a lot</strong> of iteration, I landed on this floating, fixed "Next steps" callout. Considerations for this seemingly simple component included:</p>
             <ul>
-               <li><p><strong>Context:</strong> No in-page position seemed quite right, as it's dynamic content can refer to various sections. Rather than being part of the page, this positioning lets it act as <span className='highlight'>commentary on the page itself.</span></p></li>
+               <li><p><strong>Context:</strong> No in-page position seemed quite right, as its dynamic content can refer to various sections. Rather than being part of the page, this positioning lets it act as <span className='highlight'>commentary on the page itself.</span></p></li>
                <li><p><strong>Prominence:</strong> The floating position also lets it stand out without interrupting the user's flow or the page hierarchy. It's also collapsible for the users more accustomed with the process.</p></li>
                <li><p><strong>Implementation:</strong> Our design system doesn't have this exact component, but I kept scope manageable by using our existing banner and announcement components as foundations.</p></li>
             </ul>
-            <div className='img-wrap'>
-               {/* <img src='./assets/search/mixpanel-graph.png' className='img-shadow' alt="mixpanel graph of search data" /> */}
-               <small>carousel: gif of scrolling with "next steps", status mapping, layout iterations</small>
-            </div>
+               <Swiper
+                  modules={[Navigation, Pagination]}
+                  spaceBetween={50}
+                  slidesPerView={1}
+                  navigation
+                  pagination={{ clickable: true }}
+                  onSlideChange={() => console.log('slide change')}
+                  onSwiper={(swiper) => console.log(swiper)}
+               >
+                  <SwiperSlide>
+                     <div className='img-wrap'>
+                        <img src='./assets/priorauth/status-next-steps-mapping.png' alt="prior auth full page" />
+                     </div>
+                  </SwiperSlide>
+                  <SwiperSlide>
+                     <div className='img-wrap'>
+                        <img src='./assets/priorauth/next-steps-scrolling.gif' className='img-shadow' alt="prior auth full page" />
+                     </div>
+                  </SwiperSlide>
+                  <SwiperSlide>
+                     <div className='img-wrap'>
+                        <img src='./assets/priorauth/next-steps-exploration.png' alt="final search sub results desktop" />
+                     </div>
+                  </SwiperSlide>
+               </Swiper>
             <h5>Data Entry Interactions</h5>
             <p>I came upon an interesting interaction challenge when designing the first step of the flow: marking requirements. At this step, the only column the user adjusts is "Auth required?" and <span className='highlight'>it's not uncommon that none are required.</span> This surfaces an odd pattern where the user would...</p>
             <ol>
@@ -174,8 +187,8 @@ export default function PriorAuth(){
             </ScrollAnimation>
 
             <ScrollAnimation animateIn="animate__fadeInUp-New" duration={1} animateOnce={true} delay={0}>
-            <section class='case-study-section'>
-               <h4>Final&nbsp;Designs</h4>
+            <section className='case-study-section'>
+               <h4 id='final-designs'>Final&nbsp;Designs</h4>
                <div className='desktop-swiper-wrap'>
                      <Swiper
                         modules={[Navigation, Pagination]}
@@ -222,44 +235,45 @@ export default function PriorAuth(){
             </ScrollAnimation>
 
             <ScrollAnimation animateIn="animate__fadeInUp-New" duration={1} animateOnce={true} delay={0}>
-            <section class='case-study-section'>
-            <h4>Eng Collaboration</h4>
+            <section className='case-study-section'>
+            <h4 id='eng-collab'>Eng Collaboration</h4>
             <div className='right'>
             <h5>Handoff</h5>
             <br/><br/><small>Note: Collaboration with eng certainly didn't start at handoff, these are just some highlights.</small>
             <p>As with any project, my design handoff included annotations like these throughout the figma file detailing various functionality: data sources, character limits, form logic, error states, etc.</p>
             <div className='img-wrap'>
-               {/* <img src='./assets/search/mixpanel-graph.png' className='img-shadow' alt="mixpanel graph of search data" /> */}
-               <small>image of annotation and blurb</small>
+               <img src='./assets/priorauth/eng-annotations.png' className='img-shadow' alt="annotations for engineers in figma"/>
             </div>
             <p>Since the steps of this flow can be tricky to learn, I also wrote blurbs to contextualize what the user's actions and goals are at each stage.</p>
             <p>At the initial handoff point, I recorded a walkthrough (I &#128155; Loom) of the designs and the workflow diagram in addition to doing a live walkthrough/Q&A with the engineers.</p>
-            <p>I created a weekly project sync for updates, design q's and implementation discussions, and we maintain a tight communication loop through Figma comments and Slack.</p>
+            <p>I created a weekly project sync for updates, design q's and implementation discussions, and we continue to maintain a tight communication loop through Figma comments and Slack as questions come up during implementation.</p>
             <h5>Scoping</h5>
             <p>My PM and I worked together early on to define 3 milestones for this project, considering what a large design and engineering effort it would be. Each milestone could stand on its own and would make progress toward an ideal state.</p>
             <p>I also documented detailed user stories that the engineers used to estimate initial levels of effort and a rough timeline. However, almost all our engineers were quite new to the codebase, being recent hires at the time, and our newly-formed team had gaps in our process. Partway through the project, we realized these factors had led to a significant underestimation of LOE.</p>
-            <p>Luckily, our tight communication loop helped us react quickly: I worked with our lead FE engineer to identify small pieces throughout the flow that weren't essential for release, and reduced the time to MVP by ____.</p>
+            <p>Luckily, our tight communication loop helped us react quickly: I worked with our lead FE engineer and engineering manager to identify lower impact, higher effort items that weren't essential for release, and reduced the time to MVP by ____.</p>
+            <div className='learning-outer'>
+               <div className='learning-inner'>&#128173; <small><span className='highlight'>I so appreciated the team pushing back on me</span> on the point of scope. At first, I had a hard time removing items from the MVP, but I then saw that we could make an impact significantly sooner while still working to deliver the whole scope iteratively. Being so familiar with the users' needs and what they could use a workaround for was extremely helpful here. In the future I plan to have more ruthless prioritization discussions with my PM early on.</small></div>
+            </div>
             </div>
             </section>
             </ScrollAnimation>
 
             <ScrollAnimation animateIn="animate__fadeInUp-New" duration={1} animateOnce={true} delay={0}>
-            <section class='case-study-section'>
-            <h4>What's Next?</h4>
+            <section className='case-study-section'>
+            <h4 id='next'>What's Next?</h4>
             <div className='right'>
             <h6>Now that all this prior authorization data will be recorded and stored in our database, the next step is to do something with it!</h6>
             <p>In the current workflow, when a provider submits a claim containing psychological testing services, a staff member checks whether they submitted an authorization request, if so, what the outcome was, and how that compares to the services on the claim. Now, we can work to automate this process by adding logic that checks the stored request data. This means that staff member's time can be put to better use on less tedious work.</p>
             <p>Plus, we can better educate providers about prior authorization in context and prevent them from delivering care that a patient won't be covered for.</p>
             <div className='img-wrap'>
-               {/* <img src='./assets/search/mixpanel-graph.png' className='img-shadow' alt="mixpanel graph of search data" /> */}
-               <small>image of appt slideout warning</small>
+               <img src='./assets/priorauth/appt-warnings.png' alt="annotations for engineers in figma"/>               
             </div>
             </div>
             </section>
             </ScrollAnimation>
 
             <ScrollAnimation animateIn="animate__fadeInUp-New" duration={1} animateOnce={true} delay={0}>
-            <section class='case-study-section'>
+            <section className='case-study-section'>
             <h4>Measuring Success</h4>
             <div className='right'>
             <p>It’s tough to quantify the success of this feature for a couple reasons:</p>
@@ -268,10 +282,6 @@ export default function PriorAuth(){
                <li><p>In general, conversion through search isn’t very meaningful, since we show results for multiple categories that wouldn’t lead to a purchase (Blog, DIY, Help, and More)</p></li>
             </ul>
             <p>That being said, one thing we know for sure is that <span className='highlight'>the number of people searching the site has nearly <b>quadrupled</b></span> with the addition of Sitewide Search. The individual searches in Store, Blog, and DIYs account for a combined average of <b>219</b> searches per day, compared to Sitewide Search’s average of <b>839</b>.</p>
-            <div className='img-wrap'>
-               <img src='./assets/search/mixpanel-graph.png' className='img-shadow' alt="mixpanel graph of search data" />
-               <small>Sitewide search (top/purple) traffic vs. Store, DIY, and Blog searches</small>
-            </div>
             </div>
             </section>
             </ScrollAnimation>
